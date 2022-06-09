7db47f679fb3c6b2dd7b5ac3d846324cf3e0190b
xXBJXx <32600934+xXBJXx@users.noreply.github.com>
2022-06-09 14:03:40 +0000

    Deploying to gh-pages from @ xXBJXx/documentation@366828abb6426336a0395836dfffa75c293363da 🚀

--- assets/GooglePlayStore.6b92882f.js
+++
@@ -1,1 +0,0 @@
-var o="/assets/GooglePlayStore.c6777bd8.png";export{o as _};
---
+++ assets/index.html.4a4588e3.js
@@ -0,0 +1,1 @@
+import{_ as a}from"./plugin-vue_export-helper.21dcd24c.js";import{o as r,c as l,a as n}from"./app.1e5e7f49.js";const t={},o=["src"];function s(e,c){return r(),l("div",null,[n("img",{src:e.$withBase("/media/wallpanel/wallpanel.png#center"),alt:"VuePress Logo"},null,8,o)])}var m=a(t,[["render",s],["__file","index.html.vue"]]);export{m as default};
